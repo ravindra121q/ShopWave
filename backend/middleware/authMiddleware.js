0b663f5066e8ba9007778c066114a65e0813c68d
Ravindra Kumar Tilotia <ravindrakumartilotia4@gmail.com>
2023-09-28 23:29:32 +0530

    Added routes and model (Backend)

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,17 @@
-const isAuth=(req,res,next)=>{
-const token
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  console.log(token);
+  if (!token) {
+    return res.json({ msg: "Please Login First" });
+  }
+  const decoded = jwt.verify(token, process.env.SecretKey);
+  if (decoded) {
+    next();
+    return;
+  }
+  return res.json({ msg: "Please Login Again" });
+};
 
-    next()
-}
+module.exports = { isAuth };
--- backend/routes/allRoutes.js
+++ backend/routes/allRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { userModel } = require("../models/userModel");
+const { isAuth } = require("../middleware/authMiddleware");
+const { cartModel } = require("../models/cartModel");
 const router = express.Router();
-
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const userExists = await userModel.findOne({ email });
@@ -12,7 +13,7 @@ router.post("/login", async (req, res) => {
       console.log(userExists);
       if (result == true) {
         const token = jwt.sign(
-          { userid: userExists._id },
+          { userId: userExists._id },
           process.env.SecretKey,
           { expiresIn: "7d" }
         );
@@ -44,6 +45,62 @@ router.post("/signup", async (req, res) => {
     });
   } catch (error) {
     res.send(error);
+  }
+});
+
+router.post("/product/cart", isAuth, async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.SecretKey);
+  const { title, category, price, description, image, rating } = req.body;
+
+  console.log(decoded);
+  const itemExists = await cartModel.findOne({ title });
+  if (itemExists) {
+    return res.json({ msg: "Item Already In The Cart" });
+  }
+  const item = new cartModel({
+    title,
+    category,
+    price,
+    description,
+    image,
+    rating,
+    userId: decoded.userId,
+  });
+  await item.save();
+  return res.json({ msg: "Item Added To The Cart" });
+});
+
+router.get("/product/cart/items", isAuth, async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.SecretKey);
+  if (decoded) {
+    const userCart = await cartModel.find({ userId: decoded.userId });
+
+    if (userCart) {
+      return res.json(userCart);
+    } else {
+      console.log(userCart);
+      return res.json("Wrong Credationals");
+    }
+  } else {
+    return res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+router.delete("/product/cart/items/:id", async (req, res) => {
+  const { id } = req.params;
+  const token = req.headers.authorization?.split(" ")[1];
+
+  const decoded = jwt.verify(token, process.env.SecretKey);
+  if (decoded) {
+    const cartItem = await cartModel.findOne({ _id: id });
+    if (cartItem) {
+      await cartModel.findByIdAndDelete({ _id: id });
+      return res.json({ msg: "Item Successfully Deleted" });
+    } else {
+      return res.json({ msg: "Item Not Found" });
+    }
   }
 });
 
